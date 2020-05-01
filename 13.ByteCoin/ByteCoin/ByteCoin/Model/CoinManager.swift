425fc974a5275cd13981afd586c59a93de3594a0
Cindy <1233wsh@gmail.com>
2020-05-01 21:33:02 +0900

    create 15.Flash-Chat

---
+++ 13.ByteCoin/ByteCoin/ByteCoin/Model/CoinManager.swift
@@ -0,0 +1,22 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by 우성화 on 24/03/2020.
+//  Copyright © 2020 우성화. All rights reserved.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    func getCoinPrice(for currency: String) {
+        print(currency)
+    }
+    
+}
---
+++ 15.Flash-Chat/Flash-Chat/Flash-Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flash-Chat
+//
+//  Created by 우성화 on 22/04/2020.
+//  Copyright © 2020 우성화. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
